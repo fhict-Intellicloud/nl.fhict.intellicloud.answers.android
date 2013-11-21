cc4e0e4c53ec879a3ba84cae16c08886f9d13fde
koekoek <sirojsneksuelk@hotmail.com>
2013-11-21 13:35:16 +0100

    Tab bar created
    Created a tab bar with two tabs. One for incoming questions and one for
    reviewing answers.

---
+++ Answers/src/nl/fhict/intellicloud/answers/TabFragmentIncomingQuestions.java
@@ -0,0 +1,26 @@
+package nl.fhict.intellicloud.answers;
+
+import nl.fhict.intellicloud.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragmentIncomingQuestions extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View v = inflater.inflate(R.layout.tab_fragment_incomming, null);
+		return v;
+
+	}
+
+}
---
+++ Answers/src/nl/fhict/intellicloud/answers/TabFragmentReviewQuestions.java
@@ -0,0 +1,26 @@
+package nl.fhict.intellicloud.answers;
+
+import nl.fhict.intellicloud.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabFragmentReviewQuestions extends Fragment {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View v = inflater.inflate(R.layout.tab_fragment_review, null);
+		return v;
+
+	}
+
+}
