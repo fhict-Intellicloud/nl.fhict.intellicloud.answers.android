56278d16e8b79bae7e9d3e0d9e07d7ac2cf23a26
Rouge89 <roooie_nl@hotmail.com>
2013-12-05 13:07:29 +0100

    Design fixed + sliding menu added + icons list added

--- Answers/src/nl/fhict/intellicloud/answers/IncomingQuestionsListAdapter.java
+++ Answers/src/nl/fhict/intellicloud/answers/IncomingQuestionsListAdapter.java
@@ -1,14 +1,18 @@
 package nl.fhict.intellicloud.answers;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 import nl.fhict.intellicloud.R;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class IncomingQuestionsListAdapter extends ArrayAdapter<Question> {
@@ -27,22 +31,56 @@ public class IncomingQuestionsListAdapter extends ArrayAdapter<Question> {
 	        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	    View rowView = inflater.inflate(R.layout.listview_item_incoming, parent, false);
 	    
-	    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 	    Question question = values.get(position);
 	    
+	    ImageView questionStageImage = (ImageView) rowView.findViewById(R.id.incommingQuestionStateImage);
+	    questionStageImage.setImageResource(getImageId(question.getQuestionState()));
 	    TextView firstLine = (TextView) rowView.findViewById(R.id.firstLine);
 	    firstLine.setText(question.getQuestion());
 	    TextView secondLine = (TextView) rowView.findViewById(R.id.secondLine);
-	    
-	    if(question.getDate() == null){
-	    	secondLine.setText(R.string.unknown_date);
-	    }
-	    else
-	    {
-	    	secondLine.setText(sdf.format(question.getDate()));
-	    }
-	    
+	    secondLine.setText(setTime(question.getDate()));   
 
 	    return rowView;
 	  }
+	  
+	  private int getImageId(QuestionState state){
+		  
+		  switch(state){
+		  	case Open:
+		  		return R.drawable.search_icon_green;
+		case Closed:		  
+				return R.drawable.rejected_icon_green;
+			case UpForAnswer:
+				return R.drawable.hourglass_icon_green;
+			case UpForFeedback:
+				return R.drawable.glass_icon_green;
+			  }
+		return -1;
+		  
+	  }
+	  private String setTime(Date creationDate) {
+		if(creationDate == null){
+			  return getContext().getResources().getString(R.string.unknown_date);
+		}
+		Date currentDate = new Date();
+      	long diffInMillisec = currentDate.getTime() - creationDate.getTime();
+			long diffInSec = TimeUnit.MILLISECONDS.toSeconds(diffInMillisec);
+			diffInSec/= 60;
+			long minutes =diffInSec % 60;
+			diffInSec /= 60;
+			long hours = diffInSec % 24;
+			diffInSec /= 24;
+			long days = diffInSec;
+			
+			if(days > 0){
+				return Long.toString(days) + " " + getContext().getResources().getString(R.string.daysAgo);
+			}
+			if(hours > 0){
+				return Long.toString(hours) + " " + getContext().getResources().getString(R.string.hoursAgo);
+			}
+			if(minutes > 0){
+				return Long.toString(minutes) + " " + getContext().getResources().getString(R.string.minutesAgo);
+			}
+			return getContext().getResources().getString(R.string.justPosted);
+     }
 }
--- Answers/src/nl/fhict/intellicloud/answers/MainActivity.java
+++ Answers/src/nl/fhict/intellicloud/answers/MainActivity.java
@@ -1,219 +1,283 @@
 package nl.fhict.intellicloud.answers;
 
-import java.util.Locale;
-
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import nl.fhict.intellicloud.R;
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.ParseException;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.text.method.DateTimeKeyListener;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * 
- * @author Joris & Remco
- * 
- * Class containing the TabListener for two tabs.
- * The first tab is a TabFragmentIncomingQuestions fragment.
- * The second tab is a TabFragmentReviewQuestions fragment.
- *
- */
-public class MainActivity extends FragmentActivity implements
-		ActionBar.TabListener {
-	
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+public class MainActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private String[] mFilterTitles;
 
-	
-	/**
-	 * Perform initialization of all fragments and loaders.
-	 * 
-	 * @param savedInstanceState If the activity is being re-initialized after previously being shut down then 
-	 * this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle). Note: Otherwise it is null.
-	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+        mFilterTitles = getResources().getStringArray(R.array.filter_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mFilterTitles));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+                ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         // The action bar home/up action should open or close the drawer.
+         // ActionBarDrawerToggle will take care of this.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+        case R.id.action_websearch:
+            // create intent to perform web search for this planet
+            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
+            // catch event that there's no activity to handle intent
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
+            }
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /* The click listner for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new ListFragment();
+        Bundle args = new Bundle();
+        args.putInt(ListFragment.ARG_FILTER_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mFilterTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /**
+     * Fragment that appears in the "content_frame", shows a planet
+     */
+    public static class ListFragment extends Fragment {
+        public static final String ARG_FILTER_NUMBER = "filter_number";
+
+        public ListFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_list, container, false);
+            int i = getArguments().getInt(ARG_FILTER_NUMBER);
+            String filter = getResources().getStringArray(R.array.filter_array)[i];
+
+            getActivity().setTitle(filter);
+            return rootView;
+        }
+        
+		@Override
+    	public void onViewCreated(View view, Bundle savedInstanceState) {
+    		super.onViewCreated(view, savedInstanceState);
+    		
+    		ListView lv = (ListView)getActivity().findViewById(R.id.lvIncomingQuestions);
+    	    
+    	    
+    	    int i = getArguments().getInt(ARG_FILTER_NUMBER);
+    	    ArrayList<Question> list = createListWithFilter(createDummyQuestions(), i);
+    	    IncomingQuestionsListAdapter iqla = new IncomingQuestionsListAdapter(getView().getContext(), list);
+    	    lv.setAdapter(iqla);
+    	}
 		
-
-		// Set up the action bar.
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		
-
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-		
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
+		private ArrayList<Question> createListWithFilter(ArrayList<Question> listToFilter, int filterId) {
+			ArrayList<Question> list = new ArrayList<Question>();
+			switch(filterId) {
+			case 0:
+				list = listToFilter;
+				break;
+			case 1:
+				for(Question q : listToFilter){
+					if(q.getQuestionState().equals(QuestionState.Open)){
+						list.add(q);
 					}
-				});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
-		}
-	}
-	
-	/**
-	 * Called when a tab enters the selected state.
-	 * 
-	 * @param tab The tab that was reselected.
-	 * @param fragmentTransaction A FragmentTransaction for queuing fragment operations to execute during a tab switch. 
-	 * The previous tab's unselect and this tab's select will be executed in a single transaction. 
-	 * This FragmentTransaction does not support being added to the back stack.
-	 */
-	@Override
-	public void onTabSelected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-		// When the given tab is selected, switch to the corresponding page in
-		// the ViewPager.
-		//mViewPager.setCurrentItem(tab.getPosition());
-		int position = tab.getPosition();
-		
-		Fragment fragment = null;
-		
-		switch(position) {
-			case 0:
-				fragment = new TabFragmentIncomingQuestions();
+				}
 				break;
-			
-			case 1:
-				fragment = new TabFragmentReviewQuestions();
+			case 2:
+				for(Question q : listToFilter){
+					if(q.getQuestionState().equals(QuestionState.Closed)){
+						list.add(q);
+					}
+				}
 				break;
-				
-			default:
+			case 3:
+				for(Question q : listToFilter){
+					if(q.getQuestionState().equals(QuestionState.UpForAnswer)){
+						list.add(q);
+					}
+				}
 				break;
-			
-		}
-		android.support.v4.app.FragmentTransaction fft = getSupportFragmentManager().beginTransaction();
-		fft.replace(R.id.pager, fragment);
-		//fragmentTransaction.add(R.id.pager, fragment, null);
-	}
-
-	/**
-	 * Called when a tab exits the selected state.
-	 * 
-	 * @param tab The tab that was reselected.
-	 * @param fragmentTransaction A FragmentTransaction for queuing fragment operations to execute during a tab switch. 
-	 * This tab's unselect and the newly selected tab's select will be executed in a single transaction. 
-	 * This FragmentTransaction does not support being added to the back stack.
-	 */
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	/**
-	 * Called when a tab that is already selected is chosen again by the user. 
-	 * Some applications may use this action to return to the top level of a category.
-	 * 
-	 * @param tab The tab that was reselected.
-	 * @param fragmentTransaction A FragmentTransaction for queuing fragment operations to execute once this method returns. 
-	 * This FragmentTransaction does not support being added to the back stack.
-	 */
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		/**
-		 * Return the Fragment associated with a specified position.
-		 * 
-		 * @param position The position within this adapter.
-		 */
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// the first page is for incoming questions
-			// the second page is for reviewing answers
-			
-			Fragment fragment = null;
-			
-			switch(position) {
-			
-				case 0:
-					fragment = new TabFragmentIncomingQuestions();
-					break;
-				
-				case 1:
-					fragment = new TabFragmentReviewQuestions();
-					break;
-					
-				default:
-					break;
+			case 4:
+				for(Question q : listToFilter){
+					if(q.getQuestionState().equals(QuestionState.UpForFeedback)){
+						list.add(q);
+					}
+				}
+				break;
 			}
-			return fragment;
+			return list;
 		}
-
-		/**
-		 * Returns the total number of pages within the adapter.
-		 */
-		@Override
-		public int getCount() {
-			// Show 2 total pages.
-			return 2;
-		}
-
-		/**
-		 * Returns the title of the page at a position.
-		 * 
-		 * @param position The position within this adapter.
-		 */
-		@Override
-		public CharSequence getPageTitle(int position) {			
-			//Return the page title for each tab
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.tabbarname_incomingquestions).toUpperCase(l);
-			case 1:
-				return getString(R.string.tabbarname_reviewquestions).toUpperCase(l);
-			}
+        public ArrayList<Question> createDummyQuestions() {
+        	
+    		ArrayList<Question> list = new ArrayList<Question>();
+    		list.add(new Question(1, "What does the fox say?", null, null, QuestionState.Open, new Date()));
+    		list.add(new Question(2, "What is love?", null, null, QuestionState.Open, new Date()));
+    		list.add(new Question(3, "Do you know the muffin man?", null, null, QuestionState.Open, new Date()));
+    		list.add(new Question(4, "What is your name?", null, null, QuestionState.Open, new Date()));
+    		list.add(new Question(5, "What is your quest?", null, null, QuestionState.Open, null));
+    		list.add(new Question(6, "What is the airspeed velocity of an unladen swallow?", null, null, QuestionState.Open, setDate("2013-12-04 09:27:37")));
+    		list.add(new Question(1, "Does looking at a picture of the sun hurt your eyes?", null, null, QuestionState.Closed, setDate("2013-12-05 09:27:37")));
+    		list.add(new Question(2, "How can I lose weight without moving?", null, null, QuestionState.Closed, new Date()));
+    		list.add(new Question(3, "How do I get accepted into Hogwarts?", null, null, QuestionState.UpForAnswer, new Date()));
+    		list.add(new Question(4, "Is it posible to make 1+1=3?", null, null, QuestionState.UpForAnswer, new Date()));
+    		list.add(new Question(5, "HOW DO I TURN OF CAPSLOCK?", null, null, QuestionState.UpForFeedback, null));
+    		list.add(new Question(6, "Is this an oke question?", null, null, QuestionState.UpForFeedback, new Date()));
+    		return list;
+    	}
+        
+        private Date setDate(String date) {
+        	String dtStart = date;  
+        	SimpleDateFormat  format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        	Date myDate;
+        	try {
+        		return format.parse(dtStart);
+        	} catch (Exception e) {  
+        	    // TODO Auto-generated catch block  
+        	    e.printStackTrace();  
+        	}
 			return null;
-		}
-	}
+        }
+    }
 }
